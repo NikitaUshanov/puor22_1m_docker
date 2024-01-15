964602eca6357923dfbf11c5bf201d38295f0d34
Nikita Ushanov <64222243+NikitaUshanov@users.noreply.github.com>
2024-01-15 15:47:27 +0300

    Add files via upload

---
+++ 2_task/python_app/my_spark.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+
+spark = (SparkSession
+         .builder
+         .master("spark://spark-master:7077")
+         .appName("spark_in_compose")
+         .config("spark.jars", "file:/opt/bitnami/spark/driver/postgresql-42.2.27.jre6.jar")
+         .config("spark.driver.extraClassPath", "file:/opt/bitnami/spark/driver/postgresql-42.2.27.jre6.jar")
+         .getOrCreate())
+
+sc = spark.sparkContext
+
+jdbc_url = "jdbc:postgresql://postgres:5432/spark_db?serverTimezone=UTC"
+df_prices = (
+    spark.read
+    .format("jdbc")
+    .option("url", jdbc_url)
+    .option("dbtable", "public.prices")
+    .option("user", "postgres")
+    .option("driver", "org.postgresql.Driver")
+    .option("password", "postgres")
+    .load()
+)
+
+df_prices.createOrReplaceTempView("prices")
+
+result_df = spark.sql("""
+    SELECT property_type, location, bedrooms, CEILING(AVG(price)) as average_price
+    FROM prices
+    GROUP BY property_type, location, bedrooms
+""")
+result_df.show()
